d8af0d260e75dc50e91edd56702f2f3bda4009df
Ablacodeur <agbolivicky@gmail.com>
2023-07-13 18:55:04 +0000

    drum exercice

---
+++ Drum Kit Starting Files/index.js
@@ -0,0 +1,56 @@
+var NumberOfDrumButtom = document.querySelectorAll('.drum').length
+
+for( i = 0 ; i < NumberOfDrumButtom ; i++){
+    document.querySelectorAll('button')[i].addEventListener('click', function (){
+       // alert('I got a cliked');
+        
+       var buttonInnerHtml = this.innerHTML
+
+        switch (buttonInnerHtml) {
+            case "w":
+                var tom1 = new Audio('sounds/tom-1.mp3');
+                tom1.play();  
+            break;
+
+            case "a":
+                var tom2 = new Audio('sounds/tom-2.mp3');
+                tom2.play();  
+            break;
+
+            case "s":
+                var tom3 = new Audio('sounds/tom-3.mp3');
+                tom3.play();  
+            break;
+
+            case "d":
+            var crash = new Audio('sounds/crash.mp3');
+            crash.play();  
+            break;
+
+            case "j":
+            var kick = new Audio('sounds/kick-bass.mp3');
+            kick.play();  
+            break;
+
+            case "k":
+            var snare = new Audio('sounds/snare.mp3');
+            snare.play();  
+            break;
+
+            case "l":
+            var tom4 = new Audio('sounds/tom-4.mp3');
+            tom4.play();  
+            break;
+        
+            default: console.log(buttonInnerHtml);
+        }
+
+
+    });//making eventlistened to the button
+
+   
+    
+}
+
+
+//
